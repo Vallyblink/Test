09d5c106aea3e8f776efea053bb943537af90dff
Vallyblink <alabama.toj@gmail.com>
2023-10-24 00:56:35 +0300

    test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router basename="/test">
+  <Router basename="/Test">
   <App />
 </Router>
 );
