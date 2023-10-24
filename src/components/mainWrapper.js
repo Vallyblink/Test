f4b6b4b0bc00be4c74d63612c0fd31b03fcb8d49
Vallyblink <alabama.toj@gmail.com>
2023-10-24 14:46:36 +0300

    Main

--- src/components/mainWrapper.js
+++ src/components/mainWrapper.js
@@ -1,51 +1,94 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
-import { fetchDataRequest, fetchDataSuccess, fetchDataFailure } from '../redux/dataSlice.js';
-import { useDispatch } from 'react-redux';
+import data from '../data.json';
 
 const Wrapper = styled.div`
-  background-position: center;
-  background-repeat: no-repeat;
   display: flex;
+  flex-wrap: wrap;
   justify-content: space-between;
-  align-items: center;
-  width: 2520px; /* 630px * 4 */
-  height: 400px;
+  align-items: flex-end;
+  width: 100%;
+  max-width: 1300px;
+  margin: 0 auto;
+  padding: 0 10px;
+  box-sizing: border-box;
+  overflow: auto;
 `;
 
-const DataContainer = styled.div`
+const DataBlock = styled.div`
+  background-size: cover;
+  background-repeat: no-repeat;
+  width: 630px;
+  height: 400px;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  box-sizing: border-box;
+  background-color: #ccc;
+  position: relative;
+  transition: background-color 0.3s;
+  cursor: pointer;
 
+  &:hover {
+    background-color: rgba(0, 0, 0, 0.5);
+  }
 `;
 
 const Text = styled.div`
   font-family: 'Merriweather', serif;
   color: #fff;
+  padding: 10px;
+  box-sizing: border-box;
   width: 100%;
+  text-align: left;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const TextItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin-right: 20px;
+`;
+
+const Title = styled.h1`
+    margin-bottom: 10px;
+    width: 100%;
+    text-align: left;
+    position: absolute;
+    bottom: 102px;
+    left: 15px;
+    color: #fff;
 `;
 
 function MainWrapper() {
- const dispatch = useDispatch()
-  useEffect(() => {
-    dispatch(fetchDataRequest());
-
-    fetch('/data')
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch(fetchDataSuccess(data));
-      })
-      .catch((error) => {
-        dispatch(fetchDataFailure(error));
-      });
-  }, [dispatch]);
-  
   return (
     <Wrapper>
-      <DataContainer>
-        <Text>
-          <h1>The chemical negatively charged</h1>
-          <p>Numerous calculations predict, and experiments confirm, that the force field reflects the beam, while the mass defect is not formed. The chemical compound is negatively charged. Twhile the mass defect is </p>
-        </Text>
-      </DataContainer>
+      {data.map((item, index) => (
+        <DataBlock
+          key={index}
+          style={{ backgroundImage: `url(${item.image_url})` }}
+        >
+          <Title>{item.name}</Title>
+          <Text>
+            <TextItem>
+              <p>{item.price}</p>
+              <p>Ticket - {item.ticket}</p>
+            </TextItem>
+            <TextItem>
+              <p>Yield {item.yield}</p>
+              <p>Days left {item.days_left}</p>
+            </TextItem>
+            <TextItem>
+              <p>Sold {item.sold}</p>
+            </TextItem>
+          </Text>
+        </DataBlock>
+      ))}
     </Wrapper>
   );
 }
